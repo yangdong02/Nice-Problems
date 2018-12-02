ea2f80d6febc64a23b452985a1bffc8dfcc76f17
YangDong <42331224+Yang2002@users.noreply.github.com>
2018-12-02 23:32:55 +0800

    Update BZOJ2038小Z的袜子_莫队_分块.cpp

--- Data-Structure/BZOJ2038小Z的袜子_莫队_分块.cpp
+++ Data-Structure/BZOJ2038小Z的袜子_莫队_分块.cpp
@@ -1,39 +1,50 @@
-// 莫队例题，但是不知道为什么跑得这么慢，10000+ ms
 #include<bits/stdc++.h>
 #define rep(i, n) for(register int i = 1; i <= n; ++i)
 using namespace std;
 typedef long long ll;
+  
+template<typename T> void read(T &x){
+    x = 0; int f = 1; char ch = getchar();
+    while(!isdigit(ch) ) { if(ch == '-') f = -1; ch = getchar();}
+    while( isdigit(ch) ) {x = x * 10 + ch - 48; ch = getchar();}
+    x *= f;
+}
+template<typename T, typename... Args> void read(T& x, Args&... args)
+{ read(x), read(args...); }
+template<typename T> void write(T x) {
+    if(x < 0) putchar('-'), write(-x);
+    if(x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
 
 const int maxn = 50500, maxB = 500;
 int a[maxn], n, m, L[maxB], R[maxB], pos[maxn], t = 0, lenb;
 int cnt[maxn], lenq[maxn];
 ll ans[maxn], cur_ans = 0, num[maxn], dem[maxn];
-
+  
 struct Query { int l, r, id; } q[maxn];
 inline bool cmp1(const Query &q1, const Query &q2) {return q1.l < q2.l; }
-inline bool cmp2(const Query &q1, const Query &q2) {return q1.r < q2.r; }
-
+inline bool cmp2(const Query &q1, const Query &q2) {
+    return pos[q1.l] < pos[q2.l] || (pos[q1.l] == pos[q2.l] && q1.r < q2.r); 
+}
 ll gcd(ll x, ll y) { return y == 0LL ? x : gcd(y, x % y); }
-
-inline void Ins(int v) { cur_ans += (2 * cnt[v] + 1), cnt[v]++; }
-inline void Del(int v) { cur_ans -= (2 * cnt[v] - 1), cnt[v]--; }
+  
+#define Ins(v) { cur_ans += (2 * cnt[v] + 1), cnt[v]++; }
+#define Del(v) { cur_ans -= (2 * cnt[v] - 1), cnt[v]--; }
 
 int main() {
-    scanf("%d%d", &n, &m);
-    rep(i, n) scanf("%d", &a[i]);
+    read(n, m);
+    rep(i, n) read(a[i]);
     rep(i, m) {
-        scanf("%d%d", &q[i].l, &q[i].r);
+        read(q[i].l, q[i].r);
         q[i].id = i;
         lenq[i] = q[i].r - q[i].l + 1;
     }
     sort(q + 1, q + m + 1, cmp1);
     t = sqrt(m + 0.5), lenb = m / t;
-    for(int i = 1; i <= t; ++i) {
-        L[i] = (i - 1) * lenb + 1;
-        R[i] = i * lenb;
-    }
-    if(R[t] < m) t++, L[t] = R[t-1] + 1, R[t] = m;
-    for(int i = 1; i <= t; ++i) sort(q + L[t], q + R[t] + 1, cmp2);
+    for(int i = 1; i <= m; ++i)
+        pos[i] = (i - 1) / lenb + 1;
+    sort(q + 1, q + m + 1, cmp2);
     for(int i = q[1].l; i <= q[1].r; ++i) cnt[a[i]]++;
     for(int i = 1; i <= 50000; ++i) cur_ans += 1LL * cnt[i] * cnt[i];
     ans[q[1].id] = cur_ans;
@@ -42,7 +53,7 @@ int main() {
         int nl = q[qn].l, nr = q[qn].r;
         if(cur_r < nr) for(int i = cur_r + 1; i <= nr; ++i) Ins(a[i]);
         if(cur_r > nr) for(int i = cur_r; i > nr; --i) Del(a[i]);
-        
+          
         if(cur_l > nl) for(int i = cur_l - 1; i >= nl; --i) Ins(a[i]);
         if(cur_l < nl) for(int i = cur_l; i < nl; ++i) Del(a[i]);
         ans[q[qn].id] = cur_ans;
@@ -55,7 +66,8 @@ int main() {
         gc = gcd(num[i], dem[i]);
         num[i] /= gc, dem[i] /= gc;
         if(num[i] == 0) dem[i] = 1;
-        printf("%lld/%lld\n", num[i], dem[i]);
+        write(num[i]), putchar('/');
+        write(dem[i]), putchar('\n');
     }
     return 0;
 }
